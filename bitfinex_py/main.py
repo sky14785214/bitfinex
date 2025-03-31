f021d583914e40ee3887fd91cd4455762f2b8b18
Leo <sky14785214@gmail.com>
2025-04-01 01:42:08 +0800

    fix: bug

--- bitfinex_py/main.py
+++ bitfinex_py/main.py
@@ -4,16 +4,17 @@ import datetime
 from threading import Thread
 
 class AutoLendingBitfinex:
-    def __init__(self, api_key, api_secret):
+    def __init__(self, api_key, api_secret, currency):
         self.client = Client(api_key=api_key, api_secret=api_secret)
-        self.lowest_price = 0.00025
+        self.currency = currency  # 新增變數來儲存幣種
+        self.lowest_price = 0.006
         self.set_aside_funds = 0
         self.unit_amount = 150
         self.runner_pass = False
         self.init = False
 
     def main(self):
-        print("放貸機器人 ver 1.00")
+        print(f"放貸機器人 ver 1.00 - 幣種: {self.currency}")
         while True:
             if not self.init:
                 self.task = Thread(target=self.main_runner)
@@ -28,20 +29,20 @@ class AutoLendingBitfinex:
         print("運轉中...")
         # 獲取資產餘額
         balances = self.client.rest.auth.get_wallets()
-        usd_remaining = next((x['balanceAvailable'] for x in balances if x['type'] == 'funding' and x['currency'] == 'USD'), 0)
-        usd_remaining -= self.set_aside_funds  # 扣掉使用者想要預留的資金
+        remaining = next((x.get_balance_available() for x in balances if x.get_type() == 'funding' and x.get_currency() == 'USD'), 0)
+        remaining -= self.set_aside_funds  # 扣除使用者想要預留的資金
 
-        # 如果USD剩餘有超過150
-        if usd_remaining >= self.unit_amount:
+        # 如果剩餘有超過150
+        if remaining >= self.unit_amount:
             if self.get_active_funding_offers_count() == 0:
                 # 借出金額
-                quantity = self.unit_amount if usd_remaining - self.unit_amount >= self.unit_amount else round(usd_remaining, 3) - 0.001
+                quantity = self.unit_amount if remaining - self.unit_amount >= self.unit_amount else round(remaining, 3) - 0.001
                 # 限定利率
                 rate = self.get_avg()
                 # 設定日期
                 period = self.set_period(rate)
                 if rate != 0:
-                    submit_funding_offer = self.client.rest.auth.submit_offer('fUSD', quantity, rate, period)
+                    submit_funding_offer = self.client.rest.auth.submit_offer(f'f{self.currency}', quantity, rate, period)
                     if 'id' in submit_funding_offer:
                         print(f"已送出融資訂單 , Rate {rate} day {period}")
                     else:
@@ -57,7 +58,7 @@ class AutoLendingBitfinex:
         self.main_runner()
 
     def get_active_funding_offers_count(self):
-        active_funding_offers = self.client.rest.auth.get_active_offers('fUSD')
+        active_funding_offers = self.client.rest.auth.get_active_offers(f'f{self.currency}')
         if active_funding_offers:
             if len(active_funding_offers) > 0 and round(self.get_avg(), 6) != round(float(active_funding_offers[0]['rate']), 6):
                 print(f"已有訂單 利率: {active_funding_offers[0]['rate']} 天數: {active_funding_offers[0]['period']}")
@@ -69,13 +70,13 @@ class AutoLendingBitfinex:
             return -1
 
     def get_avg(self):
-        kline_data = self.client.rest.public.get_candles('fUSD', '30m', start=datetime.datetime.utcnow() - datetime.timedelta(hours=12))
+        kline_data = self.client.rest.public.get_candles(f'f{self.currency}', '30m', start=datetime.datetime.utcnow() - datetime.timedelta(hours=12))
         if kline_data:
             high_price = sorted([x['high'] for x in kline_data], reverse=True)[:11]
             avg = sum(high_price) / len(high_price)
             print(avg)
             # 檢查最新價格是不是比avg還高
-            trade_history = self.client.rest.public.get_trades('fUSD', start=datetime.datetime.utcnow() - datetime.timedelta(minutes=30))
+            trade_history = self.client.rest.public.get_trades(f'f{self.currency}', start=datetime.datetime.utcnow() - datetime.timedelta(minutes=30))
             if trade_history:
                 if trade_history[0]['price'] > avg:
                     avg = trade_history[0]['price']
@@ -160,5 +161,6 @@ class AutoLendingBitfinex:
 if __name__ == "__main__":
     api_key = input("請輸入API 金鑰 : ")
     api_secret = input("請輸入API 密鑰 : ")
-    bot = AutoLendingBitfinex(api_key, api_secret)
+    currency = input("請輸入幣種 (USDT 或 USD) : ").upper()
+    bot = AutoLendingBitfinex(api_key, api_secret, currency)
     bot.main()
